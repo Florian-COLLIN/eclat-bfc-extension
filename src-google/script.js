c44647a98018b12dc4f05393ee59f1eb0caf9675
Florian COLLIN <florian.collin83@gmail.com>
2022-01-17 14:18:39 +0100

    Update script.js

--- src-google/script.js
+++ src-google/script.js
@@ -1,3 +1,7 @@
 // i18n
 title = chrome.i18n.getMessage("extName");
 description = chrome.i18n.getMessage("extDescription");
+
+function() {
+    var otherdevices = document.getElementById("services-sublist.js-services-sublist")
+}
