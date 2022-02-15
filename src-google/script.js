760274ca41074a92a96b02d8a3c3c4b4fa0980ba
Florian COLLIN <florian.collin83@gmail.com>
2022-02-15 14:06:54 +0100

    Update script.js

--- src-google/script.js
+++ src-google/script.js
@@ -2,7 +2,8 @@
 title = chrome.i18n.getMessage("extName");
 description = chrome.i18n.getMessage("extDescription");
 
-let otherdevices = document.createElement("li");
-otherdevices.class = "services-sublist__item";
-otherdevices.textContent = "Better Eclat-BFC";
-document.getElementById("0").appendChild(otherdevices);
+// Ajoute "Better Eclat-BFC" avec un lien vers la documentation dans l'onglet "Autres services"
+let betterLink = document.createElement("div");
+betterLink.id = "qqch";
+document.getElementById("0").appendChild(betterLink);
+document.getElementById("qqch").innerHTML = "<li class\"services-sublist__item\"><a href=\"https://florian-collin.github.io/eclat-bfc-extension/\">Better Eclat-BFC</a></li>";
