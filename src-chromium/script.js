a4277598432aef87f3b3523947298b2586f96264
Florian COLLIN <florian.collin83@gmail.com>
2022-03-19 11:45:06 +0100

    Update manifest and js file

--- src-chromium/script.js
+++ src-chromium/script.js
@@ -6,10 +6,15 @@ description = chrome.i18n.getMessage("extDescription");
 let betterLink = document.createElement("div");
     betterLink.id = "qqch";
 document.getElementById("0").appendChild(betterLink);
-document.getElementById("qqch").innerHTML = "<li class=\"services-sublist__item\"><a href=\"https://florian-collin.github.io/eclat-bfc-extension/\">Better Eclat-BFC</a></li>";
+document.getElementById("qqch").innerHTML = "<li class=\"services-sublist__item\"><a href=\"https://florian-collin.github.io/eclat-bfc-extension/\">Better Eclat-BFC</a></li><li class=\"services-sublist__item\"><a href=\"#\" onclick=\"javascript:open_Changelog();\">Journal des modifications</a></li>";
 
 // Ajoute "Better Eclat-BFC" avec un lien vers la documentation dans la page d'accueil, pour ceux qui n'ont pas l'onglet "Autres services"
 let betterLinkH = document.createElement("div");
     betterLinkH.id = "qqch2";
 document.getElementById("js-actualite-feed-content").appendChild(betterLinkH);
 document.getElementById("qqch2").innerHTML = "<hr class=\"divider divider--sm divider--full-panel\"><div class=\"text--center\"><a href=\"https://florian-collin.github.io/eclat-bfc-extension/\">Better Eclat-BFC</a> - <a href=\"https://github.com/Florian-COLLIN/eclat-bfc-extension/issues/new\">Signaler un problème</a></div>";
+
+// Changelog
+function open_Changelog() {
+    window.open('html/changelog.html');
+}
